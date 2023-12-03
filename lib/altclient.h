29307b07eb22972d39d3df6fa0b482c3068e5740
Kuzin Alexey <kuzin.alexey.96@gmail.com>
2023-12-03 16:02:44 +0300

    The client structure has been created

---
+++ lib/altclient.h
@@ -0,0 +1,26 @@
+#ifndef ALTCLIENT_H
+#define ALTCLIENT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct _altclient_t altclient_t;
+
+struct _altclient_t
+{
+    char * parent;
+};
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ALTCLIENT_H
