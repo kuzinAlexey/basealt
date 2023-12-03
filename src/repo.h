a6de15b017a13e5b749288392d5836de6d6454fe
Kuzin Alexey <kuzin.alexey.96@gmail.com>
2023-12-03 18:50:25 +0300

    Added files for the implementation of the repository object

---
+++ src/repo.h
@@ -0,0 +1,17 @@
+#ifndef REPO_H
+#define REPO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // REPO_H
