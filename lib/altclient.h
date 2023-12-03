1ab6e35fb376d9dc69a30a907f282b22281ed46c
Kuzin Alexey <kuzin.alexey.96@gmail.com>
2023-12-03 19:43:11 +0300

    The functions are marked for export

--- lib/altclient.h
+++ lib/altclient.h
@@ -11,6 +11,8 @@ extern "C" {
 
 #include <curl/curl.h>
 
+#include "altclient_global.h"
+
 #define DEFAULT_DOMAIN          (const char *)"https://rdb.altlinux.org"
 #define DEFAULT_API             (const char *)"api"
 
@@ -21,13 +23,13 @@ struct _altclient_t
     char * parent;      ///< full domain api
 };
 
-altclient_t * altclient_new(const char * domain);
-void altclient_destructor(void * cli_p);
+ALTCLIENT_EXPORT altclient_t * altclient_new(const char * domain);
+ALTCLIENT_EXPORT void altclient_destructor(void * cli_p);
 
 /**
  * 
 */
-int altclient_get_branch_binary_packages(altclient_t * cli, const char * branch, char ** response);
+ALTCLIENT_EXPORT int altclient_get_branch_binary_packages(altclient_t * cli, const char * branch, char ** response);
 
 #ifdef __cplusplus
 }
