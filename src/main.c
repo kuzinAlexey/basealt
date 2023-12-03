f7eda0365f26b4e9506c0b9a2c546dd9470d3f2d
Kuzin Alexey <kuzin.alexey.96@gmail.com>
2023-12-03 18:32:09 +0300

    Parsing command line arguments

--- src/main.c
+++ src/main.c
@@ -9,16 +9,66 @@
 
 #include "altclient.h"
 
-int main(void) 
+void print_usage();
+
+char * progname = NULL;
+
+int main(int argc, char ** argv) 
 {
-    altclient_t * cli = altclient_new( DEFAULT_DOMAIN );     
+    int rc = 0;
+    altclient_t * cli = NULL;
+    char * resp = NULL;
+    const char * repo1 = NULL;
+    const char * repo2 = NULL;
+
+//----------------------------------------------------------------------
+//  Parse args
+//----------------------------------------------------------------------
+    char * delim_ptr = strrchr(argv[0], '/');
+    progname = (delim_ptr != NULL) ? strdup(delim_ptr + 1) : strdup(argv[0]);
+    assert(progname);
+    if(argc != 3)
+    {
+        print_usage();
+        exit(EXIT_FAILURE);
+    }
+    repo1 = (const char *)argv[1];
+    repo2 = (const char *)argv[2];
+
+//----------------------------------------------------------------------
+
+    cli = altclient_new( DEFAULT_DOMAIN );     
     assert(cli);
+
+//----------------------------------------------------------------------
+//  Load packages by repo 1
+//----------------------------------------------------------------------
+    fprintf(stdout, "Load a list of packages for the repository %s \n",
+            repo1);
+    rc = altclient_get_branch_binary_packages(cli, repo1 /*"sisyphus"*/, &resp);
+    if(rc < 0)
+    {
+        fprintf(stderr, "Error load packages list %d \n", rc);
+        altclient_destructor(cli);
+        cli = NULL;
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(stdout, "LOad packet\n %s \n", resp);
+//----------------------------------------------------------------------
 
 
 
     altclient_destructor(cli);
     cli = NULL;
-    
+    free(progname);
+
     return 0;
 }
 
+
+void print_usage()
+{
+    fprintf(stdout, "Usage:  %s repo1 repo2 \n",
+            progname);
+}
