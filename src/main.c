12121aa7c33bbc4a9cc910899bc935c4b0b85782
Kuzin Alexey <kuzin.alexey.96@gmail.com>
2023-12-04 22:02:23 +0300

    Json file generation
    For each type of architecture, check for packages in repository 2 and then in repository 1.
    Generate a json file with package lists for each architecture.

--- src/main.c
+++ src/main.c
@@ -76,9 +76,10 @@ int main(int argc, char ** argv)
 //----------------------------------------------------------------------
     alt_repo_t * repo_2 = alt_repo_new();
     assert(repo_2);
-    
+
     fprintf(stdout, "Loading a list of packages by the repository %s \n",
-            repo_name_1);
+            repo_name_2);
+
     rc = altclient_get_branch_binary_packages(cli, repo_name_2, &resp);
     if(rc < 0)
     {
@@ -100,8 +101,69 @@ int main(int argc, char ** argv)
 
 //----------------------------------------------------------------------
 
+    struct json_object * only_repo_1 = json_object_new_object();
+    assert(only_repo_1);
+    struct json_object * only_repo_2 = json_object_new_object();
+    assert(only_repo_2);
+
+    alt_pack_t * pack_find, * pack;
+
+    // цикл для перебора архитектур
+    for(i = 0; i < NUM_ARCHS; i++)
+    {
+        // перебор пакетов из репозитория 1
+        struct json_object * only_arch_1 = json_object_new_array();
+        assert(only_arch_1);
+
+        pack = alt_arch_first( repo_1->archs[i] );
+        while(pack)
+        {
+            pack_find = alt_arch_find( repo_2->archs[i], pack->key);
+            if(!pack_find)
+            {
+                json_object_array_add(only_arch_1, json_object_new_string(pack->key));
+            }
+            pack = alt_arch_next( repo_1->archs[i] );
+        }
+        json_object_object_add(only_repo_1, alt_arch_string((alt_arch_id)i), only_arch_1);
+
+        // перебор пакетов из репозитория 2
+        struct json_object * only_arch_2 = json_object_new_array();
+        assert(only_arch_2);
+
+        pack = alt_arch_first( repo_2->archs[i] );
+        while(pack)
+        {
+            pack_find = alt_arch_find( repo_1->archs[i], pack->key);
+            if(!pack_find)
+            {
+                json_object_array_add(only_arch_2, json_object_new_string(pack->key));
+            }
+            pack = alt_arch_next( repo_2->archs[i] );
+        }
+        json_object_object_add(only_repo_2, alt_arch_string((alt_arch_id)i), only_arch_2);
+
+    }
+
+    struct json_object * output = json_object_new_object();
+    assert(output);
+    json_object_object_add(output, repo_name_1, only_repo_1);
+    json_object_object_add(output, repo_name_2, only_repo_2);
+
+    FILE * outfile = fopen("output.json", "w");
+    if(outfile)
+    {
+        fprintf(outfile, "\n%s\n", json_object_to_json_string(output));
+        fclose(outfile);
+    }
+
+
+    // destructors
+    json_object_put(output);
+
+    alt_repo_destructor(repo_1);
+    alt_repo_destructor(repo_2);
     altclient_destructor(cli);
-    cli = NULL;
     free(progname);
 
     return 0;
